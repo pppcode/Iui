998a523232ebac4b6bad88bee08a34c2adbcd0d9
ppp <974270397@qq.com>
2019-07-09 13:53:17 +0800

    加入 parcel, 将 button 组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="g-button">文字</button>
+</template>
+<script>
+  export default {}
+</script>
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em; /* 不固定宽度,左右padding和字体大小相关 */
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+
+</style>
