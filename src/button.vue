b833627de2fb4b79d14a7a15b5f3371e564bbff9
ppp <974270397@qq.com>
2019-07-09 17:11:49 +0800

    添加loading状态

--- src/button.vue
+++ src/button.vue
@@ -3,6 +3,7 @@
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
     <!--  拆分icon组件  -->
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -40,6 +41,11 @@
   }
 </script>
 <style lang="scss">
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
@@ -61,6 +67,9 @@
     &.icon-right {
       > .content { order: 1; }
       > .icon { order: 2; margin-right: 0; margin-left: .3em; }
+    }
+    .loading {
+      animation: spin 2s infinite linear;
     }
   }
 
