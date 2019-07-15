e15192b8d713e436b6c7ad68c8ee59c213540eab
ppp <974270397@qq.com>
2019-07-15 15:59:41 +0800

    添加 toast 和 plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header'
 import Content from './content'
 import Footer from './footer'
 import Sider from './sider'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,8 @@ Vue.component('g-header', Header)
 Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-sider', Sider)
+Vue.component('g-toast',Toast)
+Vue.use(plugin)
 
 new Vue({
   el:'#app',
@@ -38,8 +42,8 @@ new Vue({
   },
 
   methods: {
-    inputChange(e) {
-      console.log(e)
+    showToast() {
+      this.$toast('我是 message')
     }
   }
 })
