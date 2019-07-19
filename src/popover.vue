1d81db889098f3243dbd4d08892f1f8364317f28
ppp <974270397@qq.com>
2019-07-19 14:10:04 +0800

    解决抖动 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click="onClick" ref="popover">
+  <div class="popover" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible"
          :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
@@ -15,7 +15,39 @@
     name: "GuluPopover",
     data() {
       return {
-        visible: false
+        visible: false,
+      }
+    },
+    mounted () {
+      if (this.trigger === 'click') {
+        this.$refs.popover.addEventListener('click', this.onClick)
+      } else {
+        this.$refs.popover.addEventListener('mouseenter', this.open)
+        this.$refs.popover.addEventListener('mouseleave', this.close)
+      }
+    },
+    destroyed () {
+      if (this.trigger === 'click') {
+        this.$refs.popover.removeEventListener('click', this.onClick)
+      } else {
+        this.$refs.popover.removeEventListener('mouseenter', this.open)
+        this.$refs.popover.removeEventListener('mouseleave', this.close)
+      }
+    },
+    computed: {
+      openEvent () {
+        if (this.trigger === 'click') {
+          return 'click'
+        } else {
+          return 'mouseenter'
+        }
+      },
+      closeEvent () {
+        if (this.trigger === 'click') {
+          return 'click'
+        } else {
+          return 'mouseleave'
+        }
       }
     },
     props: {
@@ -25,8 +57,16 @@
         validator (value) {
           return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
         }
+      },
+      trigger: {
+        type: String,
+        default: 'click',
+        validator(value) {
+          return ['click', 'hover'].indexOf(value) >= 0
+        }
       }
     },
+
     methods: {
       positionContent () {
         const {contentWrapper, triggerWrapper} = this.$refs
@@ -79,6 +119,7 @@
         }
       }
     },
+
   }
 </script>
 <style scoped lang=scss>
