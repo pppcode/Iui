8fe2df36efee11ae0c1c7d70726b1c70e7b60d0a
ppp <974270397@qq.com>
2019-07-19 10:02:15 +0800

    功能方面基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper"
-         v-if="visible">
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
       <slot></slot>
     </span>
-
-
   </div>
 </template>
 
@@ -21,31 +18,41 @@
       }
     },
     mounted() {
-      console.log(this.$refs.triggerWrapper)
     },
     methods: {
-      xxx() {
-        this.visible = !this.visible
-        console.log('切换visible')
-        if (this.visible === true) {
-          this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper)
-            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            console.log(width, height, top, left)
-            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-            let eventHandler = () => {
-              console.log('新增监听器')
-              this.visible = false
-              console.log('document 隐藏 popover')
-              document.removeEventListener('click', eventHandler)
-              console.log('删除监听器')
-            }
-            document.addEventListener('click', eventHandler)
-          })
-        } else {
-          console.log('vm 隐藏 popover')
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        this.close()
+      },
+      open () {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close () {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick(event) {
+        if(this.$refs.triggerWrapper.contains(event.target)){
+          if (this.visible === true) {
+            this.close()
+          }else {
+            this.open()
+          }
         }
+
+
       }
     },
   }
