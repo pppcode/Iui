10f3226c06f6393590e520da3568c5992aa90c90
ppp <974270397@qq.com>
2019-07-19 16:33:37 +0800

    collapse 基本样式和基本功能完成

---
+++ src/collapse.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="collapse">
+    <div>
+      {{title}}
+    </div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GuluCollapse"
+  }
+</script>
+
+<style scoped lang=scss>
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+    /*border-bottom: none;*/
+  }
+
+</style>
