acc2943fc63722cbc56cd1e0ceb1d7caf07f74a3
ppp <974270397@qq.com>
2019-07-22 16:44:07 +0800

    创建官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  title: 'Iui',
+  description: '这是一个 Vue UI 框架',
+  themeConfig: {
+    nav: [
+      {text: '主页', link: '/'},
+      {text: '文档', link: '/guide/'},
+      {text: '交流', link: 'https://google.com'},
+    ],
+    sidebar: [
+      {
+        title: '入门',
+        collapsable: false,
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        collapsable: false,
+        children: [
+          '/components/button'
+        ]
+      }
+    ]
+  }
+}
